de3dbee090b0510c8ecb5dfe351d9081999d69ba
CAPTAIN MORGAN <136248466+CcAPtAinMORGAn@users.noreply.github.com>
2024-08-20 11:12:28 +0000

    CalculateSquareParameter.py

--- FunctionParametersPython/CalculateSquareParameter.py
+++ FunctionParametersPython/CalculateSquareParameter.py
@@ -1,9 +1,9 @@
-# def calculate_square(number):
-#     return number * number
+def calculate_square(number):
+    return number * number
 
-# # Using the function to assign a value to a variable
-# number = 7
-# square_of_number = calculate_square(number)
+# Using the function to assign a value to a variable
+number = 7
+square_of_number = calculate_square(number)
 
-# print(f"The square of {number} is {square_of_number}") 
+print(f"The square of {number} is {square_of_number}") 
 
