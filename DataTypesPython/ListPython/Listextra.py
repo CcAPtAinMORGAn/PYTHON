7a43cb3367b07386bcaab827dd68796e334736ed
CAPTAIN MORGAN <136248466+CcAPtAinMORGAn@users.noreply.github.com>
2024-10-07 19:58:07 +0530

    Create Listextra.py

---
+++ DataTypesPython/ListPython/Listextra.py
@@ -0,0 +1,36 @@
+a=[10,20,30,10,True,'Yash']
+
+# print(a)
+# print(type(a))
+# print(a[0])
+# print(a[0:4])
+# ---------------------
+# a[0]=100
+# print(a)
+# ---------------------
+# a.append(1000)
+# print(a)
+# ---------------------
+# a.insert(1,1100) 
+# print(a)
+# ---------------------
+# a.clear()
+# print(a)
+# ---------------------
+# z=a.copy()
+# print(a)
+# --------------------
+# print(a.count(10)) #Amount of times 10 appeared in list
+# print(a.index(10)) # Index of number 10 in list a
+# print(a.index(10,2)) # Find Index of number 10 after index 2
+#---------------------
+# b=[456,952,455]
+# a.extend(b)
+# print(a)
+#---------------------
+b=[456,952,455]
+a=a+b
+print(a)
+#---------------------
+
+
