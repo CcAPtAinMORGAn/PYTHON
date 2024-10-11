34ccd3304e851ecf60eff25ce0e34efd4c3f50ed
CAPTAIN MORGAN <136248466+CcAPtAinMORGAn@users.noreply.github.com>
2024-10-11 13:35:32 +0530

    Update ZeroDivisionError.py

--- ErrorPython/ZeroDivisionError.py
+++ ErrorPython/ZeroDivisionError.py
@@ -1,8 +1,9 @@
-a=int(input(" Enter any number"))
-b=int(input(" Enter any number"))
+a=int(input("Enter any number: "))
+b=int(input("Enter any number: "))
 c=a/b
-print("Result:",c)
+print(c)
 
+# When we divide (a) by zero:-----This error appears
 # Traceback (most recent call last):
 #   File "<main.py>", line 3, in <module>
 # ZeroDivisionError: division by zero
