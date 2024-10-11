ecde45712675c6476cb3e10f90ea15aac0bd24c8
CAPTAIN MORGAN <136248466+CcAPtAinMORGAn@users.noreply.github.com>
2024-10-11 13:46:24 +0530

    Create RuntimeError.py

---
+++ ErrorPython/RuntimeError.py
@@ -0,0 +1,11 @@
+try:
+    a=int(input("Enter any number: "))
+    b=int(input("Enter any number: "))
+    c=a/b
+    print("Result:",c)
+except:
+    print("Runtime Error")
+
+#Output 
+# Enter any number: a
+# Runtime Error
