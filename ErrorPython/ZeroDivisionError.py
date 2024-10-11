58f99f3aa916b248f99fcb25b888811b879898d9
CAPTAIN MORGAN <136248466+CcAPtAinMORGAn@users.noreply.github.com>
2024-10-11 13:28:37 +0530

    Update ZeroDivisionError.py

--- ErrorPython/ZeroDivisionError.py
+++ ErrorPython/ZeroDivisionError.py
@@ -2,3 +2,9 @@ a=int(input(" Enter any number"))
 b=int(input(" Enter any number"))
 c=a/b
 print("Result:",c)
+
+# Traceback (most recent call last):
+#   File "<main.py>", line 3, in <module>
+# ZeroDivisionError: division by zero
+
+# === Code Exited With Errors ===
