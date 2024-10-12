b00d603c564545e2615b014f1b19a1467217c834
CAPTAIN MORGAN <136248466+CcAPtAinMORGAn@users.noreply.github.com>
2024-10-12 19:52:53 +0530

    Create OopsExample.py

---
+++ ObjectPython/OopsExample.py
@@ -0,0 +1,16 @@
+class Body:
+    def hand(self):
+        print("For Writting")
+    def Leg(self):
+        print("For Walking")
+    def Mouth(self):
+        print("For eating and Speaking:")
+    def eye(self):
+        print("For Watching")
+
+
+ob=Body()
+ob.Leg()
+ob.eye()
+ob.hand()
+ob.Mouth()
