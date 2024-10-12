62763689ab71796def18844311be32af6ce4b76c
CAPTAIN MORGAN <136248466+CcAPtAinMORGAn@users.noreply.github.com>
2024-10-12 19:45:42 +0530

    Create AllMathFunc.py

---
+++ MathModulePython/AllMathFunc.py
@@ -0,0 +1,18 @@
+import math
+print("sqrt:",math.sqrt(25))
+print("pow:",math.pow(5,2))
+print("ceil:",math.ceil(25.01))
+print("floor:",math.floor(25.999))
+print("pi:",math.pi)
+print("sin:",math.sin(90))
+print("tan:",math.tan(45))
+print("cos:",math.cos(90))
+print("log:",math.log(100))
+print("log10:",math.log10(100))
+print("exp:",math.exp(1))
+print("hypot:",math.hypot(3,4))
+
+x=float(input("enter any number"))
+y=float(input("enter any number"))
+s=x**2 + y**2
+print("Result:",math.sqrt(s))
