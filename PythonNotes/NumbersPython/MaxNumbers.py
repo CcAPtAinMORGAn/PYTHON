9fc69fe7bb1eb35eb7eaf82afa1b52a2b27a9ccd
CAPTAIN MORGAN <136248466+CcAPtAinMORGAn@users.noreply.github.com>
2024-10-16 13:57:05 +0530

    Create MaxNumbers.py

---
+++ PythonNotes/NumbersPython/MaxNumbers.py
@@ -0,0 +1,7 @@
+# The max() method returns the largest of its 
+# arguments i.e. thevalueclosest to positive
+# infinity.
+print ("max(80, 100, 1000) : ", max(80, 100, 1000))
+print ("max(-20, 100, 400) : ", max(-20, 100, 400))
+print ("max(-80, -20, -10) : ", max(-80, -20, -10))
+print ("max(0, 100, -400) : ", max(0, 100, -400))
