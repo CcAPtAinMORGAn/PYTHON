c646e542cb0f5046128b6ee187c766df4ffa44f8
CAPTAIN MORGAN <136248466+CcAPtAinMORGAn@users.noreply.github.com>
2024-10-16 13:47:45 +0530

    Create mathFloorNumbers.py

---
+++ PythonNotes/NumbersPython/mathFloorNumbers.py
@@ -0,0 +1,6 @@
+# The floor() method returns the floor of x i.e. the largest integer not greater than x.
+import math # This will import math module
+print ("math.floor(-45.17) : ", math.floor(-45.17))
+print ("math.floor(100.12) : ", math.floor(100.12))
+print ("math.floor(100.72) : ", math.floor(100.72))
+print ("math.floor(math.pi) : ", math.floor(math.pi))
