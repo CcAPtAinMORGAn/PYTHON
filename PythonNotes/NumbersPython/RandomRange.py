2c1da58f03bb40eaba5de41e9a3310b8f3a50971
CAPTAIN MORGAN <136248466+CcAPtAinMORGAn@users.noreply.github.com>
2024-10-16 14:04:49 +0530

    Update RandomRange.py

--- PythonNotes/NumbersPython/RandomRange.py
+++ PythonNotes/NumbersPython/RandomRange.py
@@ -1,3 +1,10 @@
 import random
 
 print(random.randrange(1, 10))
+
+# import random
+# print ("returns a random number from range(100) : ",random.choice(range(100)))
+# print ("returns random element from list [1, 2, 3, 5, 9]) : ", random.choice([1,
+# 2, 3, 5, 9]))
+# print ("returns random character from string 'Hello World' : ",
+# random.choice('Hello World'))
