3055ad66a0c28e66f8ce1eee8805f29b3c5bf48c
CAPTAIN MORGAN <136248466+CcAPtAinMORGAn@users.noreply.github.com>
2024-10-16 13:43:11 +0530

    Create expNumbers.py

---
+++ PythonNotes/NumbersPython/expNumbers.py
@@ -0,0 +1,7 @@
+# The exp() method returns exponential of x: e^x
+# Here value of e = 2.718
+import math # This will import math module
+print ("math.exp(-45.17) : ", math.exp(-45.17))
+print ("math.exp(100.12) : ", math.exp(100.12))
+print ("math.exp(100.72) : ", math.exp(100.72))
+print ("math.exp(math.pi) : ", math.exp(math.pi))
