da2a97ad0a01f7270dbf15bed2391646766b727e
CAPTAIN MORGAN <136248466+CcAPtAinMORGAn@users.noreply.github.com>
2024-10-16 14:05:47 +0530

    Update RandomRange.py

--- PythonNotes/NumbersPython/RandomRange.py
+++ PythonNotes/NumbersPython/RandomRange.py
@@ -8,3 +8,9 @@ print(random.randrange(1, 10))
 # 2, 3, 5, 9]))
 # print ("returns random character from string 'Hello World' : ",
 # random.choice('Hello World'))
+
+# import random
+# # randomly select an odd number between 1-100
+# print ("randrange(1,100, 2) : ", random.randrange(1, 100, 2))
+# # randomly select a number between 0-99
+# print ("randrange(100) : ", random.randrange(100))
