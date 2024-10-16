0a9680f2c43eeb3c37ceb2521bb0b8764cb710e7
CAPTAIN MORGAN <136248466+CcAPtAinMORGAn@users.noreply.github.com>
2024-10-16 13:49:36 +0530

    Create logxNumbers.py

---
+++ PythonNotes/NumbersPython/logxNumbers.py
@@ -0,0 +1,5 @@
+# The log() method returns the natural logarithm of x, for x > 0.
+import math # This will import math module
+print ("math.log(100.12) : ", math.log(100.12))
+print ("math.log(100.72) : ", math.log(100.72))
+print ("math.log(math.pi) : ", math.log(math.pi))
