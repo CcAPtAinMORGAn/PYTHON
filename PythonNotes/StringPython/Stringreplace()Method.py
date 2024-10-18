5b093c269eefd5d45d42c35c27ae1e03afe46fa1
CAPTAIN MORGAN <136248466+CcAPtAinMORGAn@users.noreply.github.com>
2024-10-18 17:58:21 +0530

    Create Stringreplace()Method.py

---
+++ PythonNotes/StringPython/Stringreplace()Method.py
@@ -0,0 +1,4 @@
+str = "this is string example....wow!!! this is really string"
+print (str.replace("is", "was"))
+print (str.replace("is", "was", 3))
+# str.replace("is", "was", 3) replaces "is" with "was" only 3 times
