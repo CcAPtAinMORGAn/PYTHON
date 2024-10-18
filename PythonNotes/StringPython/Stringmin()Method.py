31e1dd79f1839b23f624dd6f60287a8452a769ef
CAPTAIN MORGAN <136248466+CcAPtAinMORGAn@users.noreply.github.com>
2024-10-18 17:55:16 +0530

    Create Stringmin()Method.py

---
+++ PythonNotes/StringPython/Stringmin()Method.py
@@ -0,0 +1,4 @@
+str = "asfjbn.tutorialspoint.sgahygf"
+print ("Min character: " + min(str)+ " is the character is the character least ahead in alphabet series")
+str = "TUTORIALSPOINT"
+print ("Min character: " + min(str)+ " is the character is the character least ahead in alphabet series")
