48945b41ed65be0e81d421d19e46eb23696a8d60
CAPTAIN MORGAN <136248466+CcAPtAinMORGAn@users.noreply.github.com>
2024-10-18 14:12:37 +0530

    Update Stringislower()Method.py

--- PythonNotes/StringPython/Stringislower()Method.py
+++ PythonNotes/StringPython/Stringislower()Method.py
@@ -2,3 +2,7 @@ str = "THIS is string example....wow!!!"
 print (str.islower())
 str = "this is string example....wow!!!"
 print (str.islower())
+
+# str = "THIS IS STRING EXAMPLE....WOW!!!"
+# print (str.lower())
+
