f872c7b5507bb4b99b0d47bf7501eebd5e59d9fa
CAPTAIN MORGAN <136248466+CcAPtAinMORGAn@users.noreply.github.com>
2024-10-18 17:52:22 +0530

    Create Stringmax()Method.py

---
+++ PythonNotes/StringPython/Stringmax()Method.py
@@ -0,0 +1,5 @@
+str = "this is a string example....really!!!"
+print("Max character: " + max(str) + " is the character is the character most ahead in alphabet series")
+
+str = "this is a string example....wow!!!"
+print("Max character: " + max(str) + " is the character most ahead in alphabet series")
