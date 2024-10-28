564ddf9d2273be5b13d16b4e815cab520334ad1f
CAPTAIN MORGAN <136248466+CcAPtAinMORGAn@users.noreply.github.com>
2024-10-28 12:58:44 +0530

    Create TupleMin.py

---
+++ PythonNotes/DataTypesPython/TuplePython/TupleMin.py
@@ -0,0 +1,5 @@
+tuple1, tuple2 = ('maths', 'che', 'phy', 'bio'), (456, 700, 200)
+print ("min value element : ", min(tuple1))
+print ("min value element : ", min(tuple2))
+# For tuple1, the minimum value is determined by
+# alphabetical order, so "bio" comes first.
