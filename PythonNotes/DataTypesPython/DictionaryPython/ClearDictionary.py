ffba9758b5e225a0cf4ce8f6b1f192724fae2ab7
CAPTAIN MORGAN <136248466+CcAPtAinMORGAn@users.noreply.github.com>
2024-10-28 13:22:12 +0530

    Create ClearDictionary.py

---
+++ PythonNotes/DataTypesPython/DictionaryPython/ClearDictionary.py
@@ -0,0 +1,6 @@
+dict = {'Name': 'Zara', 'Age': 7}
+print ("Start Len : %d" % len(dict))
+dict.clear()
+print ("End Len : %d" % len(dict))
+# The initial length of the dictionary is 2 because it
+# contains two key-value pairs.
