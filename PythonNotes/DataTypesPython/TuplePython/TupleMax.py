ebc1313833e0dea6df02992066eadef1636a78f7
CAPTAIN MORGAN <136248466+CcAPtAinMORGAn@users.noreply.github.com>
2024-10-28 12:56:07 +0530

    Create TupleMax.py

---
+++ PythonNotes/DataTypesPython/TuplePython/TupleMax.py
@@ -0,0 +1,5 @@
+tuple1, tuple2 = ('maths', 'che', 'phy', 'bio'), (456, 700, 200)
+print("Max value element : ", max(tuple1))
+print("Max value element : ", max(tuple2))
+# For tuple1, the maximum value is determined by the
+# alphabetical order of the strings, so "phy" comes last.
