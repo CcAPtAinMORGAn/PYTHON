98082707cb888496e40ee621fc34b82f89147058
CAPTAIN MORGAN <136248466+CcAPtAinMORGAn@users.noreply.github.com>
2024-11-06 16:47:19 +0530

    Update ContinueAndBreakPython.py

--- PythonNotes/DsaPython/ContinueAndBreakPython.py
+++ PythonNotes/DsaPython/ContinueAndBreakPython.py
@@ -1,11 +1,49 @@
-###Continue in for loop
 
+# for i in range (10,0,-4):
+#     print(i)
+
+
+# for i in range(0,11,3):
+#   print(i)
+  
+  
+# st = 'python'
+# for i in st:
+#   print(i)
+  
+  
+# st = 'python'
+# for i in st[0:3]:
+#   print(i)
+
+
+# i = 1
+# while(i<=10):
+#   print(i)
+#   i=i+1
+  
+  
+# i=10
+# while(i>=0):
+#   print(i)
+#   i=i-1
+  
+  
+# st = 'python'
+# i = 0  
+# while (i < 4):
+#     print(st[i])
+#     i = i + 1  
+    
+    
 # for i in range (1,51):
 #     print("#",end='')
 #     if(i>=10 and i<=20):
 #         continue
-#     print(i)
+#     print(i)   
 
+    
+    
 # st="python"
 # # print(st)
 # for i in st:
@@ -13,18 +51,15 @@
 #         # skips h alphabet from python.
 #         continue
 #     print(i)
-
-##******************************************************************##
-
-###break in for loop
-
+    
+    
 # st="python"
 # # print(st)
 # for i in st:
 #     if(i=='h'):
 #         break
-#     print(i)
-
+#     print(i) 
+  
 # name=input("Enter Name:")
 # s="Mr "+name[0]+"."
 # for i in name:
