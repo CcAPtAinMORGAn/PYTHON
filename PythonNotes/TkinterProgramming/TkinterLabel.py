68d9d7ef2f6a0a3df24dff23da7191d6c36d91b5
CAPTAIN MORGAN <136248466+CcAPtAinMORGAn@users.noreply.github.com>
2024-11-06 15:01:04 +0530

    Create TkinterLabel.py

---
+++ PythonNotes/TkinterProgramming/TkinterLabel.py
@@ -0,0 +1,13 @@
+from tkinter import *
+root = Tk()
+
+var = StringVar()
+
+label = Label( root, textvariable=var, relief=RAISED )
+
+var.set("Hey!? How are you doing?")
+
+label.pack()
+
+root.mainloop()
+
