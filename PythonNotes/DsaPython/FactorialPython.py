8bb44d479f44dc32387c65a37562adefd8df1e23
CAPTAIN MORGAN <136248466+CcAPtAinMORGAn@users.noreply.github.com>
2024-11-08 20:26:04 +0530

    Update FactorialPython.py

--- PythonNotes/DsaPython/FactorialPython.py
+++ PythonNotes/DsaPython/FactorialPython.py
@@ -4,3 +4,15 @@ x=1
 for i in range(a,0,-1):
     x*=i
 print(x)
+
+# def fact():
+#     a = 4
+#     x = 1
+#     for i in range(a, 0, -1):
+#         x *= i
+#     # print(x)  
+#     return x  # Return the final result
+
+# # Call the fact function and print the final factorial
+# result = fact()
+# print("The factorial of 4 is:", result)
