af68e76f65623e7a1d9a7ad1c943e2c033553582
CAPTAIN MORGAN <136248466+CcAPtAinMORGAn@users.noreply.github.com>
2024-11-08 19:52:29 +0530

    Update StarPrintPython.py

--- PythonNotes/DsaPython/StarPrintPython.py
+++ PythonNotes/DsaPython/StarPrintPython.py
@@ -1,18 +1,30 @@
-# n = 5
-# for i in range(n, 0, -1):
-#     for j in range(1, i + 1):
-#         print("* ",i,j, end="")
-#     print("\n")
 
-# n = 5
-# for i in range(1,n+1):
-#     for j in range(1, i+1):
-#         print("*", end="")
-#     print("\n")
-#     #,i,j
 
-n = 5
-for i in range(n, 0, -1):
-    for j in range(1, i + 1):
-        print(j, end="")
+# 51 52 53 54 55
+# 41 42 43 44
+# 31 32 33
+# 21 22
+# 11
+
+
+#11
+#21 22
+#31 32 33
+#41 42 43 44
+#51 52 53 54 55
+
+##
+##for i in range(5,0,-1):
+##    for j in range(1,i+1):
+##        print(i,j,"  " ,end='')
+##    print("\n")
+## 
+
+
+
+
+for i in range(1,6):
+    for j in range(1,i+1):
+        print(i,j," ",end='')
     print("\n")
+
