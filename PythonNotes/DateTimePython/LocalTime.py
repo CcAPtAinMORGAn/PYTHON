107292042e0634a351a6eafc06c5c7e172603603
CAPTAIN MORGAN <136248466+CcAPtAinMORGAn@users.noreply.github.com>
2024-11-08 12:41:01 +0530

    Update LocalTime.py

--- PythonNotes/DateTimePython/LocalTime.py
+++ PythonNotes/DateTimePython/LocalTime.py
@@ -1,7 +1,6 @@
 import time
 print (time.localtime())
-print()
-print()
 
-localtime = time.asctime( time.localtime(time.time()) )
-print ("Local current time :", localtime)
+
+##localtime = time.asctime( time.localtime(time.time()) )
+##print ("Local current time :", localtime)
