5ba3724ceba7ed61d3cf93c87703d7e0bc5ae191
CAPTAIN MORGAN <136248466+CcAPtAinMORGAn@users.noreply.github.com>
2024-11-08 13:03:14 +0530

    Create TkinterCursors.py

---
+++ PythonNotes/TkinterProgramming/TkinterCursors.py
@@ -0,0 +1,10 @@
+from tkinter import *
+import tkinter
+top = Tk()
+B1 = Button(top, text ="circle", relief=RAISED,\
+ cursor="circle")
+B2 = Button(top, text ="plus", relief=RAISED,\
+ cursor="plus")
+B1.pack()
+B2.pack()
+top.mainloop()
