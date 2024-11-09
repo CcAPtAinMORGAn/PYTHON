687beff45d5bfc2801627bd3f9897525b9582b20
CAPTAIN MORGAN <136248466+CcAPtAinMORGAn@users.noreply.github.com>
2024-11-09 18:17:58 +0530

    Update TwoSumPython.py

--- PythonNotes/DsaPython/TwoSumPython.py
+++ PythonNotes/DsaPython/TwoSumPython.py
@@ -5,3 +5,16 @@ for i in range (0,4):
     for j in range (i+1,4):
         if(nums[i]+nums[j]==Target):
             print("Target is sum of index",i,"and",j,".")
+
+# nums = [2,7,11,15]
+# target = 9
+
+# nummap={}
+
+# for i in range(len(nums)):
+#     x=target-nums[i]
+#     if x in nummap:
+#         print(nummap[x],i)
+#     nummap[nums[i]]=i
+    
+        
