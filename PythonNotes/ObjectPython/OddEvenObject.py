011acb2e62386e41b39c63bb6936042889380fa3
CAPTAIN MORGAN <136248466+CcAPtAinMORGAn@users.noreply.github.com>
2024-11-11 13:49:08 +0530

    Update OddEvenObject.py

--- PythonNotes/ObjectPython/OddEvenObject.py
+++ PythonNotes/ObjectPython/OddEvenObject.py
@@ -7,11 +7,21 @@
 # print_even_numbers()
 
 
-def print_even_numbers():
-    for num in range(start,end+1):  # Loop through numbers from 1 to 100
-        if num % 2 == 0:  # Check if the number is even
-            print(num)
-start=int(input("Enter Start: "))
-end=int(input("Enter End:  "))
+# def print_even_numbers():
+#     for num in range(start,end+1):  # Loop through numbers from 1 to 100
+#         if num % 2 == 0:  # Check if the number is even
+#             print(num)
+# start=int(input("Enter Start: "))
+# end=int(input("Enter End:  "))
 
-print_even_numbers()
+# print_even_numbers()
+
+
+def oddEven(x):
+    if(x%2!=0):
+        return f"{x} is odd"
+    else:
+        return f"{x} is even"
+    
+x=int(input("Enter Number to find odd: "))
+print(oddEven(x))   
