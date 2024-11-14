990367130517ae13615b699acac6cf734c7af8ca
CAPTAIN MORGAN <136248466+CcAPtAinMORGAn@users.noreply.github.com>
2024-11-14 20:11:29 +0530

    Update Listextra.py

--- PythonNotes/DataTypesPython/ListPython/Listextra.py
+++ PythonNotes/DataTypesPython/ListPython/Listextra.py
@@ -18,7 +18,7 @@ a=[10,20,30,10,True,'Yash']
 # print(a)
 # ---------------------
 # z=a.copy()
-# print(a)
+# print(z)
 # --------------------
 # print(a.count(10)) #Amount of times 10 appeared in list
 # print(a.index(10)) # Index of number 10 in list a
