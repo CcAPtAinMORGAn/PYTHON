17496d077195fc3e41c4382681a6b946fb3c543c
CAPTAIN MORGAN <136248466+CcAPtAinMORGAn@users.noreply.github.com>
2024-11-25 17:50:52 +0530

    Update Object_lecture_1.py

--- PythonNotes/ObjectPython/Object_lecture_1.py
+++ PythonNotes/ObjectPython/Object_lecture_1.py
@@ -48,6 +48,73 @@ print(p1.x)
 
 
 
+#class myclass:
+#    def variable(self):
+#        x = 5
+#        print(x)
+#p1 = myclass()
+#p1.variable()
+
+#class myclass:
+#    def variable(self):
+#        x = 5
+#        return x
+#        
+#p1 = myclass()
+#print(p1.variable())
+
+#class myclass:
+#    def variable(self):
+#        x = 5
+#        y = 10
+#        return x + y
+#        
+#p1 = myclass()
+#print(p1.variable())
+
+
+#class myclass:
+#    def variable(self):
+#        for i in range(1,11):
+#            print(i)
+#        
+#p1 = myclass()
+#p1.variable()
+
+#class myclass:
+#    def variable(self):
+#        for i in range(1,11):
+#              for j in range(1,11):
+#                print(i,j)
+#        
+#p1 = myclass()
+#p1.variable()
+
+
+
+#class myclass:
+#    def variable(self):
+#        x=[]
+#        for i in range(1,11):
+#            x.append(i)
+#        return x
+#p1 = myclass()
+#print(p1.variable())
+
+
+class myclass:
+    def variable(self):
+        x=[]
+        for i in range(1,11):
+            for j in range(1,11):
+                x.append(i)
+                x.append(j)
+        return x
+p1 = myclass()
+print(p1.variable())
+
+
+
 
 
 
