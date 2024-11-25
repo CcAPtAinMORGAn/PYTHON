ca0cf8e358d8a88f8b5bead59d45105e07ed47e9
CAPTAIN MORGAN <136248466+CcAPtAinMORGAn@users.noreply.github.com>
2024-11-25 17:25:42 +0530

    Create Object_lecture_1.py

---
+++ PythonNotes/ObjectPython/Object_lecture_1.py
@@ -0,0 +1,66 @@
+class myclass:
+    x = 5
+p1 = myclass()
+print(p1.x)
+
+
+#class myclass:
+#     x = 5
+#     y = 10
+#     z = x + y
+#p1 = myclass()
+#print(p1.z)
+
+#class myclass:
+#     x = []
+#     for i in range(1,11):
+#         x.append(i)   
+#p1 = myclass()
+#print(p1.x)
+
+
+#class myclass:
+#     x = 0
+#     for i in range(1,11):
+#         x+=i   
+#p1 = myclass()
+#print(p1.x)
+
+#class myclass:
+#     z = 5
+#     x = 1
+#     for i in range(1,z + 1):
+#         x*=i   
+#p1 = myclass()
+#print(p1.x)
+
+
+#class myclass:
+#     x = []
+#     for i in range(1,11):
+#         for j in range(1,11):
+#             x.append(i)
+#             x.append(j)
+#             
+#p1 = myclass()
+#print(p1.x)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
