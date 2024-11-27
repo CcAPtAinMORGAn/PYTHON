7ab0bb292bea518598c60758cadfe219c0ce256d
CAPTAIN MORGAN <136248466+CcAPtAinMORGAn@users.noreply.github.com>
2024-11-27 22:03:57 +0530

    Update Object_lecture_2.py

--- PythonNotes/ObjectPython/Object_lecture_2.py
+++ PythonNotes/ObjectPython/Object_lecture_2.py
@@ -1,3 +1,26 @@
+
+class myclass:
+  def prime(self):
+    x = 10
+    
+    
+    b = 0 
+    
+    for i in range(1,x + 1):
+      if(x % i == 0):
+        b += 1
+    
+    if(b == 2):
+      return x ,"is prime"
+    else:
+      return x,"is not prime"
+    
+p1 = myclass()
+
+print(p1.prime()
+
+
+
 class myclass:
   def __init__(self,x):
     
