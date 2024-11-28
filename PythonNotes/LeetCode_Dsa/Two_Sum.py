aee494d2b4f63699334870fcf5aeaed0843cb0d3
CAPTAIN MORGAN <136248466+CcAPtAinMORGAn@users.noreply.github.com>
2024-11-28 16:27:57 +0530

    Update Two_Sum.py

--- PythonNotes/LeetCode_Dsa/Two_Sum.py
+++ PythonNotes/LeetCode_Dsa/Two_Sum.py
@@ -1,1 +1,13 @@
-a = 2
+nums = [2,7,11,15]
+
+target = 9
+
+numMap = {}
+n = len(nums)
+
+for i in range(n):
+    complement = target - nums[i]
+    if complement in numMap:
+      print(numMap[complement], i)
+    
+    numMap[nums[i]] = i
