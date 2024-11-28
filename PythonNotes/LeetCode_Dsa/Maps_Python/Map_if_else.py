3d5f0cc495deb38f79609e028f1c0f9116054dff
CAPTAIN MORGAN <136248466+CcAPtAinMORGAn@users.noreply.github.com>
2024-11-28 18:18:11 +0530

    Create Map_if_else.py

---
+++ PythonNotes/LeetCode_Dsa/Maps_Python/Map_if_else.py
@@ -0,0 +1,17 @@
+map = {}
+
+# Add key-value pairs
+map["india"] = "150"
+map["ina"] = "150"
+map["idia"] = "10"
+map["inda"] = "150"
+
+# Print the dictionary
+# print(map)
+
+
+x = "idia"
+
+# Check if a key exists in the dictionary and print the value if it exists
+if x in map:
+    print(map["idia"])
