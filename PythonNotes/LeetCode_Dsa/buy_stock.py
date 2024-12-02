dbdb30308a2083aa671f824374115bce8a4fab6c
CAPTAIN MORGAN <136248466+CcAPtAinMORGAn@users.noreply.github.com>
2024-12-02 16:32:45 +0530

    Update buy_stock.py

--- PythonNotes/LeetCode_Dsa/buy_stock.py
+++ PythonNotes/LeetCode_Dsa/buy_stock.py
@@ -14,12 +14,67 @@ buy_position = 0
 
 for i in range(n):
   if(buy_price > prices[i]):
+    
     buy_price = prices[i]
+    
     buy_position = i
             
   profit = max(profit, prices[i] - buy_price)
-
-        
+  
 print("Buy at position",buy_position)
 
 print(profit)
+
+# buy price = 7
+# profit = 0
+# profit = max(0 , 7 - 7)
+# profit = 0
+
+
+# buy price = 2
+# profit = 0
+# profit = max(0 , 2 - 2)
+# profit = 0
+
+# buy price = 1
+# profit = 0
+# profit = max(0 , 1 - 1)
+# profit = 0
+
+# buy price = 1
+# profit = 0
+# profit = max(0 , 5 - 1)
+# profit = 4
+
+# buy price = 1
+# profit = 4
+# profit = max(4 , 3 - 1)
+# profit = 4
+
+# buy price = 1
+# profit = 4
+# profit = max(4 , 4 - 1)
+# profit = 4
+
+# buy price = 1
+# profit = 4
+# profit = max(4 , 6 - 1)
+# profit = 5
+
+
+# prices = [7,2,1,5,3,4,6]
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
